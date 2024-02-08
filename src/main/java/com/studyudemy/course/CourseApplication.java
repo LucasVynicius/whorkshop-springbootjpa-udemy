976deed842a276a1e57805e7d33047e7ffebc041
Lucas Albuquerque <luuas.albuquerque23@gmail.com>
2024-02-07 23:55:50 -0300

    User entity and resource

--- src/main/java/com/studyudemy/course/CourseApplication.java
+++ src/main/java/com/studyudemy/course/CourseApplication.java
@@ -2,9 +2,11 @@ package com.studyudemy.course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class CourseApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(CourseApplication.class, args);
